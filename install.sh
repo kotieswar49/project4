deb9998c07beb4d05208cd66dee1713bfb24f612
kotieswar49 <koti.eswar225736@gmail.com>
2024-06-05 11:51:06 +0530

    first commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#installing ansible
+
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository - yes - update ppa:ansible/ansible
+sudo apt install ansible -y
